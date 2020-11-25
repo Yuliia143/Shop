8aa560a647ed96bcc498bcf26d78403b557910b9
Yuliia <yulya.humenyuk143@gmail.com>
2020-11-25 17:06:46 +0200

    Remove comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
-  // { path: 'main', component: MainViewComponent },
   { path: 'main', loadChildren: () => import('./main-view/main-view.module').then(m => m.MainViewModule) },
-  // { path: 'product/:id', component: ProductViewComponent },
   {
     path: 'product/:id',
     loadChildren: () => import('./product-view/product-view.module').then(m => m.ProductViewModule)
   },
-  // { path: 'checkout', component: CheckoutViewComponent },
   {
     path: 'checkout',
     loadChildren: () => import('./checkout-view/checkout-view.module').then(m => m.CheckoutViewModule)
