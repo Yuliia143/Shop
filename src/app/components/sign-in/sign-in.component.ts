322a5895b00605450c39c7afd6a82655d27956c6
Yuliia <yulya.humenyuk143@gmail.com>
2021-01-13 15:59:43 +0200

    Fix styles

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@shared/services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
+import { takeUntil } from "rxjs/operators";
+import { UserInterface } from "@shared/interfaces/user-interface";
 
 @Component({
     selector: 'app-sign-in',
     templateUrl: './sign-in.component.html',
     styleUrls: ['./sign-in.component.scss']
 })
+
 export class SignInComponent implements OnInit {
 
     constructor(public authService: AuthService, private dialog: MatDialog) {
